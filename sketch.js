c106e85232b2e445ab54849abb89811194e786d4
ParthKejriwal <62254837+ParthKejriwal@users.noreply.github.com>
2020-03-18 08:48:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var trex,trexRunning,trexCollided,groundImage
+function preload(){
+trexRunning=loadAnimation("trex.png");
+  trexCollided=loadAnimation("trexCollided.png");
+  groundImage=loadAnimation("ground2.png");
+}
+function setup() {
+  createCanvas(400, 400);
+  trex
+  ground
+}
+
+function draw() {
+  background(220);
+}
